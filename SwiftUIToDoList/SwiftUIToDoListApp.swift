d5ae763ea9fc1b01bd29138553305d99f0490a5e
mohammad blur <blor1379@gmail.com>
2024-05-16 20:24:17 +0330

    Initial Commit

---
+++ SwiftUIToDoList/SwiftUIToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToDoListApp.swift
+//  SwiftUIToDoList
+//
+//  Created by Mohammad Blur on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
