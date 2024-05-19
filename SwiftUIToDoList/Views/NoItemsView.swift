ab009676578ffa274ca5dcccce3531ebee9fac75
mohammad blur <blor1379@gmail.com>
2024-05-19 21:56:00 +0330

    :lipstick: add empty view and some animation

---
+++ SwiftUIToDoList/Views/NoItemsView.swift
@@ -0,0 +1,65 @@
+//
+//  NoItemsView.swift
+//  SwiftUIToDoList
+//
+//  Created by Mohammad Blur on 5/19/24.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    let accentColor = Color("SeconderyAccentColor")
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text("there are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add button and add bunch of items to your todo list!")
+                    .padding(.bottom, 20)
+                NavigationLink(
+                    destination: AddView()) {
+                        Text("Add Something 🥳")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(animate ? accentColor : Color.accentColor)
+                            .cornerRadius(10)
+                    }
+                    .padding(.horizontal, animate ? 30 : 50)
+                    .shadow(
+                        color: animate ? Color.red.opacity(0.7) : Color.accentColor.opacity(0.7),
+                        radius: animate ? 30 : 10,
+                        x: 0,
+                        y: animate ? 50 : 30
+                    )
+                    .scaleEffect(animate ? 1.1 : 1.0)
+                    .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
